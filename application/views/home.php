db3fb91373fe9e266992b980dead2e6ce304d91a
Eduardo Linares <elinares1202@gmail.com>
2014-05-21 22:14:23 -0600

    Update

---
+++ application/views/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/css.css">
+	<title>OfferMalls</title>
+</head>
+<body>
+
+<div id="container">	
+	<div>
+		<img src="<?php echo base_url();?>images/construction-logo.png" alt="OfferMalls">
+	</div>
+</div>
+
+</body>
+</html>
